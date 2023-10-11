99e93efbea7d4a895902054f6d4a39a3db81d50b
andrealemonroy <andreale17@icloud.com>
2023-10-10 22:52:00 -0500

    fix delay

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { Footer } from './Footer';
 import { Navbar } from './Navbar';
 import MegaMenu from './Megamenu';
@@ -8,14 +8,38 @@ interface LayoutProps {
 }
 
 export const Layout: FC<LayoutProps> = ({ children }) => {
-  const [openMenu, setOpenMenu] = React.useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
+  let timeoutId: NodeJS.Timeout;
+
+  const closeMenuAfterDelay = () => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      setOpenMenu(false);
+    }, 3000); // 3000 milliseconds = 3 seconds
+  };
+
+  useEffect(() => {
+    closeMenuAfterDelay();
+
+    // Add event listeners to handle user interactions
+    const handleMenuInteraction = () => {
+      setOpenMenu(true); // Set the menu as open when there's an interaction
+      closeMenuAfterDelay(); // Reset the timeout
+    };
+
+    document.addEventListener('click', handleMenuInteraction);
+
+    return () => {
+      document.removeEventListener('click', handleMenuInteraction);
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
   return (
     <div>
       <Navbar openMenu={openMenu} setOpenMenu={setOpenMenu} />
       <MegaMenu openMenu={openMenu} setOpenMenu={setOpenMenu} />
-      <div className={
-        `${openMenu ? 'hidden' : 'block'}`
-      }>{children}</div>
+      <div className={`${openMenu ? 'hidden' : 'block'}`}>{children}</div>
       <Footer />
     </div>
   );
