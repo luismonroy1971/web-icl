ae09a3cb48033c8b250764c95e5028e4b97e3ffb
andrealemonroy <andreale17@icloud.com>
2023-10-13 16:49:13 -0500

    fix search style

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,7 +15,7 @@ export const Layout: FC<LayoutProps> = ({ children }) => {
   const [openSearch, setOpenSearch] = useState(false);
   const [textSearch, setTextSearch] = useState('');
   const [results, setResults] = useState<any>([]);
-  let timeoutId: NodeJS.Timeout;
+  const [mouseOver, setMouseOver] = useState('');
   const router = useRouter();
 
   const findInData = (text: string) => {
@@ -47,7 +47,11 @@ export const Layout: FC<LayoutProps> = ({ children }) => {
 
   return (
     <div>
-      <Navbar openMenu={openMenu} setOpenMenu={setOpenMenu} setOpenSearch={setOpenSearch}/>
+      <Navbar
+        openMenu={openMenu}
+        setOpenMenu={setOpenMenu}
+        setOpenSearch={setOpenSearch}
+      />
       <MegaMenu openMenu={openMenu} setOpenMenu={setOpenMenu} />
       {openSearch && (
         <div className="fixed z-50 inset-0 overflow-y-auto">
@@ -126,10 +130,28 @@ export const Layout: FC<LayoutProps> = ({ children }) => {
                                 setOpenSearch(false);
                                 router.push(result.link);
                               }}
+                              onMouseEnter={() => setMouseOver(index)}
+                              onMouseLeave={() => setMouseOver('')}
                             >
-                              <h3 className="font-acto text-xl text-left">
-                                {result.name}
-                              </h3>
+                              <div className="flex gap-4">
+                                <span className={`font-acto text-2xl text-primary ${mouseOver === index ? 'text-white' : ''}`}>
+                                  {result.name}
+                                </span>
+                                <div className="flex items-center justify-center">
+                                  <svg
+                                    width="21"
+                                    height="16"
+                                    viewBox="0 0 21 16"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                  >
+                                    <path
+                                      d="M20.7071 8.70711C21.0976 8.31658 21.0976 7.68342 20.7071 7.29289L14.3431 0.928932C13.9526 0.538408 13.3195 0.538408 12.9289 0.928932C12.5384 1.31946 12.5384 1.95262 12.9289 2.34315L18.5858 8L12.9289 13.6569C12.5384 14.0474 12.5384 14.6805 12.9289 15.0711C13.3195 15.4616 13.9526 15.4616 14.3431 15.0711L20.7071 8.70711ZM0 9H20V7H0V9Z"
+                                      fill={mouseOver === index ? '#fff' : '#16236E'}
+                                    />
+                                  </svg>
+                                </div>
+                              </div>
                               <p className="font-lato text-lg text-left">
                                 {result.description}
                               </p>
